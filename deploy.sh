fa3b5abd6088f564929853967fdcc2653695355d
François Rullière <kapochamo@gmail.com>
2023-09-21 11:47:12 +0800

    font and cache

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ scp index.html nginx.conf   edweis@lipp.igw.world:/var/www/pantone
 ssh edweis@lipp.igw.world "sudo nginx -t && sudo nginx -s reload"
 
 
-curl -L pantone.kapochamo.com
+curl -L pantone.kapochamo.com | head 
 # Create a symlink on the server: 
-#   sudo ln -fs /var/html/pantone/nginx.conf /etc/nginx/conf.d/pantone.kapochamo.com
+#   sudo ln -fs /var/www/pantone/nginx.conf /etc/nginx/conf.d/pantone.kapochamo.com.conf
 # Restart NGINX
 #   sudo nginx -t && sudo nginx -s reload
