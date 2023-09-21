a860d52c818e3dc41ea21de579f28a306c60bcfe
François Rullière <kapochamo@gmail.com>
2023-09-21 11:42:40 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,11 @@
-scp index.html edweis@lipp.igw.world:/home/edweis/Documents/pantone
+set -ex
+
+scp index.html nginx.conf   edweis@lipp.igw.world:/var/www/pantone
+ssh edweis@lipp.igw.world "sudo nginx -t && sudo nginx -s reload"
+
+
+curl -L pantone.kapochamo.com
+# Create a symlink on the server: 
+#   sudo ln -fs /var/html/pantone/nginx.conf /etc/nginx/conf.d/pantone.kapochamo.com
+# Restart NGINX
+#   sudo nginx -t && sudo nginx -s reload
