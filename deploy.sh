e540a2f66895eae4de8175e55f460256f65a8039
François Rullière <kapochamo@gmail.com>
2024-10-22 20:46:16 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 set -ex
 
-scp index.html nginx.conf   edweis@lipp.igw.world:/var/www/pantone
-# ssh edweis@lipp.igw.world "sudo nginx -t && sudo nginx -s reload"
+PROJECT=citrouille
+USER=lipp
+SERVER=lipp.local 
+
+scp index.html nginx.conf   $USER@$SERVER:/home/$USER/projects/pantone/
+ssh $USER@$SERVER "sudo nginx -t && sudo nginx -s reload"
 
 
-curl -L pantone.kapochamo.com | head 
+# curl -L pantone.kapochamo.com | head 
 # Create a symlink on the server: 
-#   sudo ln -fs /var/www/pantone/nginx.conf /etc/nginx/conf.d/pantone.kapochamo.com.conf
+#  sudo ln -fs /home/lipp/projects/pantone/nginx.conf /etc/nginx/conf.d/pantone.kapochamo.com.conf
 # Restart NGINX
 #   sudo nginx -t && sudo nginx -s reload
